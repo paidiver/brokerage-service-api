142af5a58a32ea3186ab3ae77611b6a7c4ed2e9a
Cavan Robinson <cavrob@noc.ac.uk>
2026-07-02 14:28:46 +0100

    Update __init__.py

--- src/brokerage_service_api/api/routes/__init__.py
+++ src/brokerage_service_api/api/routes/__init__.py
@@ -1,7 +1,7 @@
 """Sub directory to store routes."""
 
+from brokerage_service_api.api.routes.export import router as export_router
 from brokerage_service_api.api.routes.search import router as brokerage_search_router
 from brokerage_service_api.api.routes.source_health import router as source_health_router
-from brokerage_service_api.api.routes.export import router as export_router
 
 __all__ = ["source_health_router", "brokerage_search_router", "export_router"]
